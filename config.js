2eb9756dc4efebe6e8172d93cee4c43c33a6c221
Karim Tayie <karimsherif2597@gmail.com>
2019-05-12 22:12:51 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2087988661250305',
+        clientSecret: 'e16ce2ca56b8731e064bb27d4127f2ec'
+    }
 }
